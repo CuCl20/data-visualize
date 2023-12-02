e1f088ee3063834bba47c4b2846cc3ee59e24048
unknown <2403683410@qq.com>
2023-12-02 11:22:32 +0800

    0.1.0

---
+++ g.py
@@ -0,0 +1,13 @@
+import geopandas as gpd
+
+# 输入 Shapefile 文件路径
+shp_path = 'wmap/ne_10m_admin_0_countries_lakes.shp'
+
+# 读取 Shapefile
+gdf = gpd.read_file(shp_path)
+
+# 输出 GeoJSON 文件路径
+geojson_path = 'output.geojson'
+
+# 将 GeoDataFrame 保存为 GeoJSON
+gdf.to_file(geojson_path, driver='GeoJSON')
